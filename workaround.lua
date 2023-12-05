49291cb17a0f306d64175c6a4193dfbec54243a4
Scully <51968381+Scullyy@users.noreply.github.com>
2023-12-04 23:56:31 -0800

    Updated
    - Prevent cheaters from abusing the deleteEntity event.
    - Use SetEntityIgnoreRequestControlFilter for servers that use sv_filterRequestControl.
    - No longer attempts to attach the object if it doesn't exist.
    - Checks for entity ownership as this can be very inconsistent with entities spawned on the server.
    - Add a timeout to prevent getting stuck on the spawnPhoneObject function.

--- workaround.lua
+++ workaround.lua
@@ -6,10 +6,19 @@ if IsDuplicityVersion() then
         local ped = GetPlayerPed(source)
         local coords = GetEntityCoords(ped)
         local object = CreateObject(model, coords.x, coords.y, coords.z, true, true)
+        local timeout, entityExists = 0, true
+
+        while not DoesEntityExist(object) do
+            Wait(0)
     
-        while not DoesEntityExist(object) do Wait(50) end
+            timeout += 1
     
-        return NetworkGetNetworkIdFromEntity(object)
+            if timeout > 2000 then entityExists = false break end
+        end
+
+        SetEntityIgnoreRequestControlFilter(object, true)
+    
+        return entityExists and NetworkGetNetworkIdFromEntity(object) or false
     end
 
     lib.RegisterCallback('lb-phone_workaround:spawnPhone', function(source, cb, model)
@@ -17,7 +26,17 @@ if IsDuplicityVersion() then
     end)
 
     RegisterNetEvent('lb-phone_workaround:deleteEntity', function(netId)
+        if GetInvokingResource() then return end
+
+        local src = source
         local entity = NetworkGetEntityFromNetworkId(netId)
+        local model = GetEntityModel(entity)
+
+        if model ~= Config.PhoneModel then
+            DropPlayer(src, 'Cheating. (Attempted to delete an entity using "lb-phone_workaround:deleteEntity")')
+
+            return
+        end
         
         DeleteEntity(entity)
     end)
@@ -34,19 +53,44 @@ else
             local coords = GetEntityCoords(ped)
 
             lib.TriggerCallback('lb-phone_workaround:spawnPhone', function(netId)
-                local timeout = 0
+                if not netId then return end
+
+                local object = NetToObj(netId)
+                local timeout, entityExists = 0, true
                 
-                while not NetworkDoesEntityExistWithNetworkId(netId) do
+                while not DoesEntityExist(object) do
                     Wait(0)
             
                     timeout += 1
             
-                    if timeout > 2000 then break end
+                    if timeout > 2000 then entityExists = false break end
                 end
 
-                phoneId = NetToObj(netId)
+                if not entityExists then return end
 
-                AttachEntityToEntity(phoneId, ped, GetPedBoneIndex(ped, 28422), Config.PhoneOffset.x, Config.PhoneOffset.y, Config.PhoneOffset.z, Config.PhoneRotation.x, Config.PhoneRotation.y, Config.PhoneRotation.z, 1, 1, 0, 0, 2, 1)
+                timeout = 0
+
+                while true do
+                    Wait(0)
+
+                    local hasControl = NetworkGetEntityOwner(object) == playerId
+
+                    if hasControl then 
+                        break
+                    else
+                        NetworkRequestControlOfEntity(object) 
+                    end
+            
+                    timeout += 1
+            
+                    if timeout > 2000 then entityExists = false break end
+                end
+
+                if entityExists then
+                    phoneId = object
+                    
+                    AttachEntityToEntity(phoneId, ped, GetPedBoneIndex(ped, 28422), Config.PhoneOffset.x, Config.PhoneOffset.y, Config.PhoneOffset.z, Config.PhoneRotation.x, Config.PhoneRotation.y, Config.PhoneRotation.z, 1, 1, 0, 0, 2, 1)
+                end
             end, Config.PhoneModel)
         else
             if DoesEntityExist(phoneId) then
